b7e24c2e202a58ec0fffb97d4f9b137f8dd66ec7
Adrian Campos <adriancampos@users.noreply.github.com>
2018-10-10 02:14:15 -0700

    Added HMAC authentication

--- README.md
+++ README.md
@@ -5,13 +5,13 @@ https://tradlab.me
 ## File Encryption
 Using Python [Cryptography](https://cryptography.io/en/latest/hazmat/primitives/), these modules encrypt and decrypt a message or file.
 ### Part 1
-`MyEncrypt(message, key)` generates a 16 byte IV and encrypts the message using key and IV in AES CBC mode.
+`MyEncrypt(message, enc_key, hmac_key)` generates a 16 byte IV and encrypts the message using key and IV in AES CBC mode.
 
-`MyDecrypt(ciphertext, key, iv)` decrypts ciphertext using a given key and iv in AES CBC mode.
+`MyDecrypt(ciphertext, enc_key, hmac_key, iv, tag)` decrypts ciphertext using a given key and iv in AES CBC mode.
 
 `MyFileEncrypt(filepath)` generates a 32 byte key. Reads the file at filepath, encrypts the contents, and writes them to (filepath + ".encrypted"). Can be modified to write the result to the original file if so desired.
 
-`MyFileDecrypt(filepath, key, iv)` reads the file at filepath, decrypts the contents, and writes them to (filepath + ".decrypted"). Can easily be modified to write the result to the original file if so desired.
+`MyFileDecrypt(filepath, enc_key, hmac_key, iv, tag)` reads the file at filepath, decrypts the contents, and writes them to (filepath + ".decrypted"). Can easily be modified to write the result to the original file if so desired.
 
 #### Try it out
 Running main() will run `test_MyEncrypt_Decrypt()` and `test_MyFileEncrypt_Decrypt()`.
@@ -21,4 +21,4 @@ Running main() will run `test_MyEncrypt_Decrypt()` and `test_MyFileEncrypt_Decry
 `test_MyFileEncrypt_Decrypt` generates a random key, reads in a file specified by `TEST_FILENAME`, encrypts and writes the contents to a new `.encrypted` file, reads that file's contents, and uses the key to decrypt its contents before writing them to a `.decrypted` file.
 
 
-To see it in action, place some text into `filetoencrypt.txt`. The first few paragraphs of [alice.txt](http://www.umich.edu/~umfandsf/other/ebooks/alice30.txt) are provided. After running the program, two new files should appear: `filetoencrypt.txt.encrypted` and `filetoencrypt.txt.encrypted.decrypted` (please do not save these to version control!). The encrypted file should be unreadable and the decrypted file should exactly match the original file. You may also try this with any binary file (remember to change `TEST_FILENAME`!)
+To see it in action, place some text into `data/filetoencrypt.txt`. The first few paragraphs of [alice.txt](http://www.umich.edu/~umfandsf/other/ebooks/alice30.txt) are provided. After running the program, two new files should appear: `filetoencrypt.txt.encrypted` and `filetoencrypt.txt.encrypted.decrypted` (please do not save these to version control!). The encrypted file should be unreadable and the decrypted file should exactly match the original file. You may also try this with any binary file (remember to change `TEST_FILENAME`!)
--- filyencryption.py
+++ filyencryption.py
@@ -2,6 +2,7 @@ import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes, hmac
 
 
 def main():
@@ -14,15 +15,17 @@ def main():
 
 def test_MyEncrypt_Decrypt():
     print("Testing encrypt(decrypt(\"Hello World!\"))")
-    key = os.urandom(32)
+    enc_key = os.urandom(32)
+    hmac_key = os.urandom(32)
 
     plaintext = b"Hello World!"
 
-    (ct, iv) = MyEncrypt(plaintext, key)
+    (tag, ct, iv) = MyEncrypt(plaintext, enc_key, hmac_key)
 
     print("ciphertext:", ct)
+    print("tag:", tag)
 
-    decryptedtext = MyDecrypt(ct, key, iv)
+    decryptedtext = MyDecrypt(ct, enc_key, hmac_key, iv, tag)
 
     print("result:", decryptedtext)
     print("Matches:", plaintext == decryptedtext)
@@ -32,31 +35,32 @@ TEST_FILENAME = "data/filetoencrypt.txt"
 
 
 def test_MyFileEncrypt_Decrypt():
-    (ciphertext, iv, key, ext) = test_MyFileEncrypt()
-    test_MyFileDecrypt(iv, key, ext)
+    (ciphertext, iv, tag, enc_key, hmac_key, ext) = test_MyFileEncrypt()
+    test_MyFileDecrypt(iv, enc_key, hmac_key, tag, ext)
 
 
 def test_MyFileEncrypt():
     print("Testing encryptfile(decryptfile(\"" + TEST_FILENAME + "\"))")
-    (ciphertext, iv, key, ext) = MyFileEncrypt(TEST_FILENAME)
-    return ciphertext, iv, key, ext
+    (ciphertext, iv, tag, enc_key, hmac_key, ext) = MyFileEncrypt(TEST_FILENAME)
+    return ciphertext, iv, tag, enc_key, hmac_key, ext
 
 
-def test_MyFileDecrypt(iv, key, ext):
-    MyFileDecrypt(TEST_FILENAME + ext, key, iv)
+def test_MyFileDecrypt(iv, enc_key, hmac_key, tag, ext):
+    MyFileDecrypt(TEST_FILENAME + ext, enc_key, hmac_key, iv, tag)
 
 
-def MyEncrypt(message, key):
+def MyEncrypt(message, enc_key, hmac_key):
     """
     Generates a 16 byte IV and encrypts the message using key and IV in AES CBC mode.
     :param message: 
-    :param key:
+    :param enc_key: 
+    :param hmac_key: 
     :raises ValueError: if len(key) < 32
-    :return: (ciphertext, IV)
+    :return: (tag, ciphertext, IV)
     """
 
     # ensure that len(key) >= 32
-    if len(key) < 32:
+    if len(enc_key) < 32:
         raise ValueError("key must be at least 32 bytes long")
 
     # pad message
@@ -68,25 +72,38 @@ def MyEncrypt(message, key):
     # encrypt padded_message
     backend = default_backend()
     iv = os.urandom(16)
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
+    cipher = Cipher(algorithms.AES(enc_key), modes.CBC(iv), backend=backend)
     encryptor = cipher.encryptor()
     ct = encryptor.update(padded_message) + encryptor.finalize()
 
-    return ct, iv
+    # HMAC
+    h = hmac.HMAC(hmac_key, hashes.SHA256(), backend=default_backend())
+    h.update(ct)
+    tag = h.finalize()
+
+    return tag, ct, iv
 
 
-def MyDecrypt(ciphertext, key, iv):
+def MyDecrypt(ciphertext, enc_key, hmac_key, iv, tag):
     """
     Decrypts ciphertext using key and iv in AES CBC mode.
-    :param ciphertext: 
-    :param key: 
-    :param iv: 
+    :param ciphertext:
+    :param enc_key: 
+    :param hmac_key: 
+    :param iv:
+    :param tag:
+    :raises cryptography.exceptions.InvalidSignature: if tag does not match digest
     :return: plaintext
     """
 
+    # HMAC verify
+    h = hmac.HMAC(hmac_key, hashes.SHA256(), backend=default_backend())
+    h.update(ciphertext)
+    h.verify(tag)
+
     # decrypt ciphertext
     backend = default_backend()
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
+    cipher = Cipher(algorithms.AES(enc_key), modes.CBC(iv), backend=backend)
     decrypter = cipher.decryptor()
     padded_plaintext = decrypter.update(ciphertext) + decrypter.finalize()
 
@@ -103,37 +120,40 @@ def MyFileEncrypt(filepath):
     Generates a 32 byte key. Reads the file at filepath, encrypts the contents, and writes them to (filepath + ".encrypted").
     Can easily be modified to write the result to the original file if so desired.
     :param filepath: 
-    :return: (ciphertext, IV, key, file extension of new file)
+    :return: (ciphertext, IV, HMAC tag, encryption key, HMAC key, file extension of new file)
     """
 
-    key = os.urandom(32)
+    enc_key = os.urandom(32)
+    hmac_key = os.urandom(32)
 
-    print("key:", key)
+    print("key:", enc_key)
 
     with open(filepath, 'rb') as file:
         # TODO The project description says that we must read the file as a string. That can cause potential problems when encrypting/decrypting raw bytes, depending on the encoding. Let's read it in binary mode unless told otherwise.
         contents = file.read()
 
-        (ciphertext, iv) = MyEncrypt(contents, key)
+        (tag, ciphertext, iv) = MyEncrypt(contents, enc_key, hmac_key)
 
     with open(filepath + ".encrypted", 'wb') as file:
         file.write(ciphertext)
 
-    return ciphertext, iv, key, ".encrypted"
+    return ciphertext, iv, tag, enc_key, hmac_key, ".encrypted"
 
 
-def MyFileDecrypt(filepath, key, iv):
+def MyFileDecrypt(filepath, enc_key, hmac_key, iv, tag):
     """
     Reads the file at filepath, decrypts the contents, and writes them to (filepath + ".decrypted").
     Can easily be modified to write the result to the original file if so desired.
     :param filepath: 
-    :param key: 
+    :param enc_key: 
+    :param hmac_key: 
     :param iv: 
+    :param tag: 
     """
     with open(filepath, 'rb') as file:
         contents = file.read()
 
-        plaintext = MyDecrypt(contents, key, iv)
+        plaintext = MyDecrypt(contents, enc_key, hmac_key, iv, tag)
 
     with open(filepath + ".decrypted", 'wb') as file:
         file.write(plaintext)
